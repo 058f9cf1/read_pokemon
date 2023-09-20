eb4c42169617356fddbc832b65305e770b333781
058f9cf1 <058f9cf1@tutanota.com>
2023-09-21 00:32:47 +0100

    Changed file detection

--- read_pk1.c
+++ read_pk1.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
 int main(int argc, char **argv)
 {
 	const int SIZE = 69;
 	unsigned char buffer[SIZE];
 	
-	if(access(argv[1], R_OK) == 0)
-	{
-		FILE *file = fopen(argv[1],"rb");
-		fread(buffer, SIZE, 1, file);
-	}
-	else
+	FILE *file = fopen(argv[1], "rb");
+	
+	if(file == NULL)
 	{
 		printf("Not a .pk1 file\n");
-    	exit(0);
+		return 1;
 	}
 
+	fread(buffer, SIZE, 1, file);
 	
 	for(int i = 0; i < SIZE; i++)
+	{
     	printf("%x ", buffer[i]);
-    printf("\n");
+	}
+	printf("\n");
     
 	
 	printf("%d\n", (buffer[4])<<8);
@@ -30,5 +28,6 @@ int main(int argc, char **argv)
 	printf("HP: %d\n", (buffer[4]<<8) | buffer[5]);
 	printf("Level: %d\n", buffer[6]);
 	printf("Status Condition: %d\n", buffer[7]);
-}
 
+	return 0;
+}
