7a216075739ac71cf113f739ceb5bfde80c02905
058f9cf1 <058f9cf1@tutanota.com>
2023-10-07 16:13:18 +0100

    Added all gen 1 data

--- src/pokemon/read_pk1.c
+++ src/pokemon/read_pk1.c
@@ -39,12 +39,13 @@ typedef struct
 	move move_4;
 
 	int catch_rate;
-	int original_trainer;
-	int nickname;
+	int original_trainer[10];
+	int nickname[10];
 
-	int gender;
+	char gender;
 	int shiny;
 }pokemon;
+
 
 int read_pk1(unsigned char* data)
 {
@@ -87,35 +88,64 @@ int read_pk1(unsigned char* data)
 	p.stat.speed = (data[43] << 8) | data[44];
 	p.stat.special = (data[45] << 8) | data[46];
 
-	//Show ot name and nickname
+	for(int i = 0; i < 10; i++)
+	{
+		p.original_trainer[i] = data[47 + i];
+		p.nickname[i] = data[58 + i];
+	}
 
-	//Calculate hp iv
-	//Calculate gender
-	//Calculate shinyness
+	p.iv.hp = ((p.iv.attack & 1) << 3) + ((p.iv.defence & 1) << 2) + ((p.iv.speed & 1) << 1) + (p.iv.special & 1);
+	p.gender = gender_list(pk1_pk2_species(p.species), p.iv.attack);
+	p.shiny = 0;
+	if(p.iv.defence == 10 && p.iv.speed == 10 && p.iv.special == 10)
+	{
+		if((p.iv.attack & 0b11) == 0b10 || (p.iv.attack & 0b11) == 0b11)
+		{
+			p.shiny = 1;
+		}
+
+	}
 
 
 	//Print out Pokemon data
-	printf("Species: %s\n", pk1_pk2_species(p.species));
-	printf("HP: %d\n", p.current_hp);
+	printf("Species: %s\n", species(pk1_pk2_species(p.species)));
+	printf("Nickname: ");
+	for(int i = 0; i < 10; i++)
+	{
+		printf("%c", pk1_character_map(p.nickname[i]));
+	}
+	if(p.type_1 == p.type_2)
+	{
+		printf("\nType: %s\n", type(p.type_1));
+	}
+	else
+	{
+		printf("\nType: %s/%s\n", type(p.type_1), type(p.type_2));
+	}
+	printf("Original Trainer: ");
+	for(int i = 0; i < 10; i++)
+	{
+		printf("%c", pk1_character_map(p.original_trainer[i]));
+	}
+	printf("\nOT ID: %d\n", p.ot_id);
+	printf("Level: %d\n", p.level);
+	printf("Exp Points: %d\n", p.exp_points);
+	printf("Current HP: %d\n", p.current_hp);
 	printf("Status Condition: %s\n", status_condition(p.status_condition));
-	printf("Type 1: %s\n", type(p.type_1));
-	printf("Type 2: %s\n", type(p.type_2));
 	printf("Catch Rate: %d\n", p.catch_rate);
-	printf("Move 1: %s\n", move_name(p.move_1.name));
+
+	printf("\n   Stat| IV |    EV | Total\n");
+	printf("-------|----|-------|------\n");
+	printf("     HP|%3d |%6d |%5d\n", p.iv.hp, p.ev.hp, p.stat.hp);
+	printf(" Attack|%3d |%6d |%5d\n", p.iv.attack, p.ev.attack, p.stat.attack);
+	printf("Defence|%3d |%6d |%5d\n", p.iv.defence, p.ev.defence, p.stat.defence);
+	printf("  Speed|%3d |%6d |%5d\n", p.iv.speed, p.ev.speed, p.stat.speed);
+	printf("Special|%3d |%6d |%5d\n", p.iv.special, p.ev.special, p.stat.special);
+	
+	printf("\nMove 1: %s\n", move_name(p.move_1.name));
 	printf("Move 2: %s\n", move_name(p.move_2.name));
 	printf("Move 3: %s\n", move_name(p.move_3.name));
 	printf("Move 4: %s\n", move_name(p.move_4.name));
-	printf("OT ID: %d\n", p.ot_id);
-	printf("Exp Points: %d\n", p.exp_points);
-	printf("HP EV: %d\n", p.ev.hp);
-	printf("Attack EV: %d\n", p.ev.attack);
-	printf("Defence EV: %d\n", p.ev.defence);
-	printf("Speed EV: %d\n", p.ev.speed);
-	printf("Special EV: %d\n", p.ev.special);
-	printf("Attack IV: %d\n", p.iv.attack);
-	printf("Defence IV: %d\n", p.iv.defence);
-	printf("Speed IV: %d\n", p.iv.speed);
-	printf("Special IV: %d\n", p.iv.special);
 	printf("Move 1 Current PP: %d\n", p.move_1.pp);
 	printf("Move 1 PP Ups: %d\n", p.move_1.pp_ups);
 	printf("Move 2 Current PP: %d\n", p.move_2.pp);
@@ -124,12 +154,11 @@ int read_pk1(unsigned char* data)
 	printf("Move 3 PP Ups: %d\n", p.move_3.pp_ups);
 	printf("Move 4 Current PP: %d\n", p.move_4.pp);
 	printf("Move 4 PP Ups: %d\n", p.move_4.pp_ups);
-	printf("Level: %d\n", p.level);
-	printf("HP Stat: %d\n", p.stat.hp);
-	printf("Attack Stat: %d\n", p.stat.attack);
-	printf("Defence Stat: %d\n", p.stat.defence);
-	printf("Speed Stat: %d\n", p.stat.speed);
-	printf("Special Stat: %d\n", p.stat.special);
+
+	printf("\nGeneration 2 features\n");
+	printf("Shiny: %d\n", p.shiny);
+	printf("Gender: %c\n", p.gender);
+	printf("Held Item: %d\n", p.catch_rate);
 
 	return 0;
 }
--- src/pokemon/tables.h
+++ src/pokemon/tables.h
@@ -5,6 +5,8 @@ const char* status_condition(int);
 const char* type(int);
 const char* move_name(int);
 const char* species(int);
-const char* pk1_pk2_species(int);
+const char gender_list(int, int);
+const char pk1_character_map(int);
+const int pk1_pk2_species(int);
 
 #endif
