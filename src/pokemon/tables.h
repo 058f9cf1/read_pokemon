5d2e667ae0f1b87727f9496c810607b13be7cb3f
058f9cf1 <058f9cf1@tutanota.com>
2023-10-11 18:40:57 +0100

    Added held items

--- src/pokemon/tables.h
+++ src/pokemon/tables.h
@@ -3,6 +3,8 @@
 
 const char* status_condition(int);
 const char* type(int);
+const char* gen_2_item(int);
+const char* catch_rate_to_item(int);
 const char* move_name(int);
 const char* species(int);
 const char gender_list(int, int);
