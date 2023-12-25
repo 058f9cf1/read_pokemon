65c1e9c58f67a2198ad234823377bccefd8e31bc
058f9cf1 <058f9cf1@tutanota.com>
2023-12-25 23:02:46 +0000

    Made printing nicer

--- src/pokemon/print_pk1.c
+++ src/pokemon/print_pk1.c
@@ -7,47 +7,48 @@
 void print_pk1(gen_1_pokemon* p)
 {
 	//Print out Pokemon data
-	printf("Species: %s\n", species(pk1_pk2_species(p->species)));
-	printf("Nickname: %s\n", p->nickname);
+	printf("\n|------------------------------------|\n");
+	printf("| #%03d %-10s  ", pk1_pk2_species(p->species), species(pk1_pk2_species(p->species)));
 	if(p->type_1 == p->type_2)
 	{
-		printf("Type: %s\n", type(p->type_1));
+		printf("%17s |\n", type(p->type_1));
 	}
 	else
 	{
-		printf("Type: %s/%s\n", type(p->type_1), type(p->type_2));
+		printf("%8s/%-8s |\n", type(p->type_1), type(p->type_2));
 	}
-	printf("Original Trainer: %s\n", p->original_trainer);
-	printf("OT ID: %d\n", p->ot_id);
-	printf("Level: %d\n", p->level);
-	printf("Exp Points: %d\n", p->exp_points);
-	printf("Current HP: %d\n", p->current_hp);
-	printf("Status Condition: %s\n", status_condition(p->status_condition));
-	printf("Catch Rate: %d\n", p->catch_rate);
+	printf("| %-10s %-22c", p->nickname, p->gender);
+	if(p->shiny)
+	{
+		printf("*");
+	}
+	else
+	{
+		printf(" ");
+	}
+	printf(" |\n| OT: %-10s TID: %05d          |\n", p->original_trainer, p->ot_id);
+	printf("| Level %-3d Exp %-20d |\n", p->level, p->exp_points);
+	printf("| HP: %3d/%-3d Status: %-14s |\n", p->current_hp, p->stat.hp, status_condition(p->status_condition));
+	printf("| Held Item: %-23s |\n", catch_rate_to_item(p->catch_rate));
+	printf("|------------------------------------|\n");
 
-	printf("\n   Stat| IV |    EV | Total\n");
-	printf("-------|----|-------|------\n");
-	printf("     HP|%3d |%6d |%5d\n", p->iv.hp, p->ev.hp, p->stat.hp);
-	printf(" Attack|%3d |%6d |%5d\n", p->iv.attack, p->ev.attack, p->stat.attack);
-	printf("Defence|%3d |%6d |%5d\n", p->iv.defence, p->ev.defence, p->stat.defence);
-	printf("  Speed|%3d |%6d |%5d\n", p->iv.speed, p->ev.speed, p->stat.speed);
-	printf("Special|%3d |%6d |%5d\n", p->iv.special, p->ev.special, p->stat.special);
+	printf("\n|---------|----|-------|-------|\n");
+	printf("|    Stat | IV |    EV | Total |\n");
+	printf("|---------|----|-------|-------|\n");
+	printf("|      HP |%3d |%6d |%6d |\n", p->iv.hp, p->ev.hp, p->stat.hp);
+	printf("|  Attack |%3d |%6d |%6d |\n", p->iv.attack, p->ev.attack, p->stat.attack);
+	printf("| Defence |%3d |%6d |%6d |\n", p->iv.defence, p->ev.defence, p->stat.defence);
+	printf("|   Speed |%3d |%6d |%6d |\n", p->iv.speed, p->ev.speed, p->stat.speed);
+	printf("| Special |%3d |%6d |%6d |\n", p->iv.special, p->ev.special, p->stat.special);
+	printf("|---------|----|-------|-------|\n");
 
-	printf("\nMove 1: %s\n", move_name(p->move_1.name));
-	printf("Move 2: %s\n", move_name(p->move_2.name));
-	printf("Move 3: %s\n", move_name(p->move_3.name));
-	printf("Move 4: %s\n", move_name(p->move_4.name));
-	printf("Move 1 Current PP: %d\n", p->move_1.pp);
-	printf("Move 1 PP Ups: %d\n", p->move_1.pp_ups);
-	printf("Move 2 Current PP: %d\n", p->move_2.pp);
-	printf("Move 2 PP Ups: %d\n", p->move_2.pp_ups);
-	printf("Move 3 Current PP: %d\n", p->move_3.pp);
-	printf("Move 3 PP Ups: %d\n", p->move_3.pp_ups);
-	printf("Move 4 Current PP: %d\n", p->move_4.pp);
-	printf("Move 4 PP Ups: %d\n", p->move_4.pp_ups);
-
-	printf("\nGeneration 2 features\n");
-	printf("Shiny: %d\n", p->shiny);
-	printf("Gender: %c\n", p->gender);
-	printf("Held Item: %s\n", catch_rate_to_item(p->catch_rate));
+	printf("\n|----------------|----------------|\n");
+	printf("| %-14s | %-14s |\n", move_name(p->move_1.name), move_name(p->move_2.name));
+	printf("|                |                |\n");
+	printf("| %10d(+%d) | %10d(+%d) |\n", p->move_1.pp, p->move_1.pp_ups, p->move_2.pp, p->move_2.pp_ups);
+	printf("|----------------|----------------|\n");
+	printf("| %-14s | %-14s |\n", move_name(p->move_3.name), move_name(p->move_4.name));
+	printf("|                |                |\n");
+	printf("| %10d(+%d) | %10d(+%d) |\n", p->move_3.pp, p->move_3.pp_ups, p->move_4.pp, p->move_4.pp_ups);
+	printf("|----------------|----------------|\n");
 }
